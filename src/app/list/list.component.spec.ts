24d9c150a1ae5ec33cbb58052d23f54163f2d1ca
Jared Martin <jared@livechurchsolutions.org>
2022-01-21 00:18:44 -0600

    Update list component

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -1,10 +1,13 @@
+import { ProviderService } from '../services/provider.service';
 import { ListComponent } from './list.component';
 
 describe('ListComponent', () => {
   let component: ListComponent;
+  const providerService: ProviderService = new ProviderService();
 
   beforeEach(() => {
-    component = new ListComponent();
+    component = new ListComponent(providerService);
+    component.ngOnInit();
   });
 
   it('should create', () => {
@@ -12,8 +15,8 @@ describe('ListComponent', () => {
   });
 
   describe('unselected providers', () => {
-    it('should have an initial length of 3', () => {
-      expect(component.unselectedProviders.length).toEqual(3);
+    it('should have an initial length of 4', () => {
+      expect(component.unselectedProviders.length).toEqual(4);
     });
 
     it('should have an id', () => {
@@ -37,5 +40,39 @@ describe('ListComponent', () => {
     it('should have no initial length', () => {
       expect(component.selectedProviders.length).toEqual(0);
     });
+
+    it('should update the selected providers list when a provider is selected', () => {
+      const selectedProvider = component.unselectedProviders[2];
+      component.selectProvider(selectedProvider);
+      expect(component.selectedProviders.length).toEqual(1);
+    });
+
+    it('should store the selected provider in localStorage', () => {
+      const selectedProvider = component.selectedProviders[0];
+      const savedProviderIds = JSON.parse(localStorage.getItem('savedProviderIds'));
+      expect(savedProviderIds[0]).toEqual(selectedProvider.id);
+    });
+
+    it('should remove the selected provider from the unselected providers list', () => {
+      expect(component.unselectedProviders.length).toEqual(3);
+    });
   });
+
+  describe('unselected providers', () => {
+    it('should remove the unselected provider from the selected providers list', () => {
+      const unselectedProvider = component.selectedProviders[0];
+      component.removeProvider(unselectedProvider);
+      expect(component.selectedProviders.length).toEqual(0);
+    });
+
+    it('should remove the unselected provider id from localStorage', () => {
+      const savedProviderIds = JSON.parse(localStorage.getItem('savedProviderIds'));
+      expect(savedProviderIds.length).toEqual(0);
+    });
+
+    it('should add the unselected provider back to the unselected providers list', () => {
+      expect(component.unselectedProviders.length).toEqual(4);
+    });
+  });
+
 });
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Provider } from '../interfaces/provider.interface';
+import { ProviderService } from '../services/provider.service';
 
 @Component({
   selector: 'app-list',
@@ -7,30 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  public selectedProviders = [];
-  public unselectedProviders = [
-    {
-      id: '1',
-      name: 'John',
-      address: '123 Greenway Blvd',
-      phone: '8991234321'
-    },
-    {
-      id: '2',
-      name: 'Mary',
-      address: '443 Windwhisper Road',
-      phone: '2233211903'
-    },
-    {
-      id: '3',
-      name: 'Jason',
-      address: '9992 Pumpkin Hollow',
-      phone: '4343219384'
+  public selectedProviders: Provider[] = [];
+  public unselectedProviders: Provider[] = [];
+
+  constructor(
+    private providerService: ProviderService
+  ) { }
+
+  ngOnInit() {
+    const allProviders = this.providerService.getProviders();
+    const savedProviderIds: string[] = JSON.parse(localStorage.getItem('savedProviderIds')) || [];
+    if (savedProviderIds.length > 0) {
+      allProviders.forEach((provider: Provider) => {
+        savedProviderIds.includes(provider.id) ? this.selectedProviders.push(provider) : this.unselectedProviders.push(provider);
+      });
+    } else {
+      this.unselectedProviders = allProviders;
     }
-  ];
+  }
 
-  constructor() {}
+  public selectProvider(provider: Provider): void {
+    this.saveProvider(provider.id);
+    this.selectedProviders.push(provider);
+    this.unselectedProviders = this.unselectedProviders.filter((unselectedProvider: Provider) => unselectedProvider.id !== provider.id);
+  }
 
-  ngOnInit() {}
+  public removeProvider(provider: Provider): void {
+    this.removeSavedProvider(provider.id);
+    this.unselectedProviders.push(provider);
+    this.selectedProviders = this.selectedProviders.filter((selectedProvider: Provider) => selectedProvider.id !== provider.id);
+  }
 
+  private saveProvider(providerId: string): void {
+    const savedProviders = JSON.parse(localStorage.getItem('savedProviderIds')) || [];
+    savedProviders.push(providerId);
+    localStorage.setItem('savedProviderIds', JSON.stringify(savedProviders));
+  }
+
+  private removeSavedProvider(providerId: string): void {
+    let savedProviders = JSON.parse(localStorage.getItem('savedProviderIds')) || [];
+    savedProviders = savedProviders.filter((id: string) => id !== providerId);
+    localStorage.setItem('savedProviderIds', JSON.stringify(savedProviders));
+  }
 }
